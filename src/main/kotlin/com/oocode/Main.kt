8fc51224683ee50d1a9162fce78a8d41fb89f27c
Yasin Usman <yasin.usman@kellogg.ox.ac.uk>
2025-04-08 14:45:20 +0100

    fixed tests

--- src/main/kotlin/com/oocode/Main.kt
+++ src/main/kotlin/com/oocode/Main.kt
@@ -4,4 +4,20 @@ fun main(args: Array<String>) {
     println("Hello World!")
 }
 
-fun moo() = "boo"
+fun calcCal(input: String): String {
+    val items = input.split("\n\n").filter { it.isNotBlank() }
+    var count = 0
+
+    for (item in items){
+        val calories = item.split("\n").mapNotNull { it.toIntOrNull() }
+        println(calories)
+        val totalCals = calories.sum()
+
+        if (totalCals > count) {
+            count = totalCals
+        }
+    }
+    return count.toString()
+}
+
+
--- src/test/kotlin/com/oocode/MainKtTest.kt
+++ src/test/kotlin/com/oocode/MainKtTest.kt
@@ -1,13 +1,55 @@
 package com.oocode
 
+import calcCal
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import moo
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-internal class MainKtTest {
+class findTotal {
+
     @Test
-    fun `moo is moo`() {
-        assertThat(moo(), equalTo("boo"))
+    fun `findTotal`() {
+        assertThat(calcCal("""1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""), equalTo("24000"))
     }
+}
+
+
+class findTotalsingleElf {
+
+
+    @Test
+    fun `findTotalsingleElf`() {
+        assertThat(calcCal("1000"), equalTo("1000"))
+    }
+
+    @Test
+    fun `findTwoElfoneitem` () {
+        assertThat(calcCal("""1000
+
+2000""".trimMargin()), equalTo("2000"))
+    }
+
+
+    @Test
+    fun findHighestElftwoitem () {
+        assertThat(calcCal("""1000
+3000
+
+2000""".trimMargin()), equalTo("5000"))
+    }
+
 }
